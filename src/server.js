835f6c7b8324de7af011ea79e13b081f0a0813b4
Nick Snyder <N_Snyder119@hotmail.com>
2020-08-13 14:25:52 -0700

    fixed config.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
+'use strict';
 const app = require('./app');
-const { PORT } = require('./config')
+const { PORT } = require('./config');
 
 
 app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`)
-})
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
